e6e5a4c1e8f327bd249481d2a79182e7d8a383df
ArshidaAfsal <arshidak2000@outlook.com>
2024-12-24 00:27:36 +0530

    initial commit

--- Book/urls.py
+++ Book/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('creator/', include("Bookapp.urls")),
-    path('user/',include("userapp.urls")),
+    path('',include("userapp.urls")),
     path('account/',include("account.urls"))
 ]
 
--- Bookapp/urls.py
+++ Bookapp/urls.py
@@ -4,7 +4,7 @@ from  . import views
 urlpatterns = [
     path('createbook',views.createBook,name='createbook'),
     path('author/',views.create_Author,name='author'),
-    path('',views.listBook,name="booklist"),
+    path('booklist',views.listBook,name="booklist"),
     path('detailview/<int:book_id>/',views.detailsview,name='details'),
     path('updateview/<int:book_id>/',views.UpdateBook,name='update'),
     path('deleteview/<int:book_id>/',views.deleteview,name='delete'),
